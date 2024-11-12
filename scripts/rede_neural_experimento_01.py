b5bfa752835cc1b1b7639530678f222e5788c374
Rafael <dalacquar@gmail.com>
2024-11-12 14:33:52 -0300

    Organizando experimento_01 e resultados

--- scripts/rede_neural_experimento_01.py
+++ scripts/rede_neural_experimento_01.py
@@ -7,7 +7,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import GridSearchCV
 
-def run_model(treino_path, teste_path):
+def read_dados(treino_path, teste_path):
     treino_df = pd.read_csv(treino_path)
     teste_df = pd.read_csv(teste_path)
 
@@ -34,6 +34,9 @@ def run_model(treino_path, teste_path):
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
 
+    return X_train, X_test, y_train, y_test
+
+def get_hyper_params_rede_neural(X_train, y_train):
     # Definir os hiperparâmetros para o Grid Search
     param_grid = {
         'max_iter':[10000],
@@ -53,6 +56,13 @@ def run_model(treino_path, teste_path):
     grid_search.fit(X_train, y_train)
     best_params = grid_search.best_params_
 
+    return best_params
+
+def run_model(treino_path, teste_path):
+    X_train, X_test, y_train, y_test = read_dados(treino_path, teste_path)
+
+    best_params = get_hyper_params_rede_neural(X_train, y_train)    
+
     # Criar e treinar o modelo com os melhores hiperparâmetros
     model = MLPClassifier(
         hidden_layer_sizes=best_params['hidden_layer_sizes'],
@@ -62,6 +72,8 @@ def run_model(treino_path, teste_path):
         max_iter=10000,
         random_state=42
     )
+
+    # Treinar modelo
     model.fit(X_train, y_train)
 
     # Fazer previsões com o conjunto de teste
@@ -69,24 +81,30 @@ def run_model(treino_path, teste_path):
 
     # Avaliar o desempenho do modelo
     accuracy = accuracy_score(y_test, y_pred)
-    return accuracy  # Retornar a acurácia em vez de imprimir
+    return accuracy
+
+def save_results_csv(results):
+    # Criar um DataFrame a partir dos resultados
+    results_df = pd.DataFrame(results)
+
+    # Salvar os resultados em um arquivo CSV
+    output_path = os.path.join(base_path, 'results', 'rede_neural_experimento_01.csv')
+    results_df.to_csv(output_path, index=False)
+
+    print(f'Resultados salvos em {output_path}')
+
 
 temporadas = [
     "2008-2009", "2009-2010", "2011-2012", "2012-2013",
     "2013-2014", "2014-2015", "2015-2016", "2016-2017",
     "2018-2019", "2019-2020", "2020-2021", "2021-2022", "2022-2023", "2023-2024"
 ]
-
 porcentagens_treino = [0.2, 0.4, 0.5, 0.6, 0.8, 0.9]
 
 base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 # Lista para armazenar resultados
 results = []
-
-
-
-
 
 # Início do temporizador
 start_time = time.time()
@@ -122,14 +140,6 @@ for porcentagem in porcentagens_treino:
         'Acurácia': f'Média: {mean_accuracy:.2f}, Desvio Padrão: {std_dev:.2f}'
     })
 
-# Criar um DataFrame a partir dos resultados
-results_df = pd.DataFrame(results)
-
-# Salvar os resultados em um arquivo CSV
-output_path = os.path.join(base_path, 'resultados_acuracias.csv')
-results_df.to_csv(output_path, index=False)
-
-print(f'Resultados salvos em {output_path}')
 
 # Fim do temporizador
 end_time = time.time()
