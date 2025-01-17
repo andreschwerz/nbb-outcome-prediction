d3542cb1db519e9692867d1420a0f8f32c3f287c
Rafael <dalacquar@gmail.com>
2025-01-16 22:25:44 -0300

    Atualizando dados experimento 2

--- utils/experimento_02.py
+++ utils/experimento_02.py
@@ -147,8 +147,8 @@ if __name__ == "__main__":
 
     filtrarPorTemporada = True
 
-    qtds_jogos_treino = [16]
-    qtds_jogos_teste = [1,2,3,4,6,8]
+    qtds_jogos_treino = [32]
+    qtds_jogos_teste = [1,2,3,4,6,8,16]
 
     base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     
