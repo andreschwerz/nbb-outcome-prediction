c541fe8aa87cb58fd3e1525d8b9597d132750062
Rafael <dalacquar@gmail.com>
2025-04-06 13:19:02 -0300

    Translating repository

--- scripts/experiment_01.py
+++ scripts/experiment_01.py
@@ -13,95 +13,92 @@ from xgboost_model import run_model_xgboost
 from experiments import save_results_csv
 
 # vanilla, neural_network, svm, random_forest, naive_bayes, xgboost
-modelo = 'xgboost'
-jogos_medias = ['5','10','15']
+model = 'xgboost'
+games_averages = ['5', '10', '15']
 
-temporadas = [
+seasons = [
     "2008-2009", "2009-2010", "2011-2012", "2012-2013",
     "2013-2014", "2014-2015", "2015-2016", "2016-2017",
     "2018-2019", "2019-2020", "2020-2021", "2021-2022", "2022-2023", "2023-2024"
 ]
 
-porcentagens_treino = [0.2, 0.4, 0.5, 0.6, 0.8, 0.9]
-# porcentagens_treino = [0.2]
+training_percentages = [0.2, 0.4, 0.5, 0.6, 0.8, 0.9]
 
 base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
-# Lista para armazenar resultados
+# List to store results
 results = []
-bests_params = []
+best_params_list = []
 
-# Início do temporizador
+# Start the timer
 start_time = time.time()
 
-for jogos_media in jogos_medias:
-    for porcentagem in porcentagens_treino:
-        acuracias_por_porcentagem = []
-        f1_scores_por_porcentagem = []
+for games_average in games_averages:
+    for percentage in training_percentages:
+        accuracies_by_percentage = []
+        f1_scores_by_percentage = []
 
-        for temporada in temporadas:
-            porcentagem_str = str(porcentagem)
+        for season in seasons:
+            percentage_str = str(percentage)
 
-            # Construir os caminhos de treino e teste usando os.path
-            treino_path = os.path.join(base_path, "data", "experiment_01", jogos_media, temporada, porcentagem_str, "treino.csv")
-            teste_path = os.path.join(base_path, "data", "experiment_01", jogos_media, temporada, porcentagem_str, "teste.csv")
+            # Build training and test paths using os.path
+            train_path = os.path.join(base_path, "data", "experiment_01", games_average, season, percentage_str, "treino.csv")
+            test_path = os.path.join(base_path, "data", "experiment_01", games_average, season, percentage_str, "teste.csv")
 
-            # Executar o modelo e armazenar a acurácia
-            print(f"Rodando modelo para temporada {temporada} e porcentagem {porcentagem_str}")
+            # Run the model and store the accuracy
+            print(f"Running model for season {season} and percentage {percentage_str}")
 
-            if(modelo == 'neural_network'):
-                accuracy, f1, best_params = run_model_neural_network(treino_path, teste_path, True)
+            if model == 'neural_network':
+                accuracy, f1, best_params = run_model_neural_network(train_path, test_path, True)
 
-            elif(modelo == 'vanilla'):
-                accuracy, f1, best_params = run_model_vanilla(treino_path, teste_path)
+            elif model == 'vanilla':
+                accuracy, f1, best_params = run_model_vanilla(train_path, test_path)
 
-            elif modelo == 'svm':
-                accuracy, f1, best_params = run_model_svm(treino_path, teste_path, True)
+            elif model == 'svm':
+                accuracy, f1, best_params = run_model_svm(train_path, test_path, True)
 
-            elif modelo == 'random_forest':
-                accuracy, f1, best_params = run_model_rf(treino_path, teste_path, True)
+            elif model == 'random_forest':
+                accuracy, f1, best_params = run_model_rf(train_path, test_path, True)
 
-            elif modelo == 'naive_bayes':
-                accuracy, f1, best_params = run_model_naive_bayes(treino_path, teste_path)
+            elif model == 'naive_bayes':
+                accuracy, f1, best_params = run_model_naive_bayes(train_path, test_path)
 
-            elif modelo == 'xgboost':
-                accuracy, f1, best_params = run_model_xgboost(treino_path, teste_path, True)
+            elif model == 'xgboost':
+                accuracy, f1, best_params = run_model_xgboost(train_path, test_path, True)
 
-            # bests_params.append(best_params)
-
-            acuracias_por_porcentagem.append(accuracy)
-            f1_scores_por_porcentagem.append(f1)
+            accuracies_by_percentage.append(accuracy)
+            f1_scores_by_percentage.append(f1)
 
             results.append({
-                'Porcentagem de Treino': porcentagem,
-                'Temporada': temporada,
-                'Acurácia': f'{accuracy:.2f}',
-                'Desvio Padrão Acurácia': '-',
+                'Training Percentage': percentage,
+                'Season': season,
+                'Accuracy': f'{accuracy:.2f}',
+                'Accuracy Std Dev': '-',
                 'F1-Score': f'{f1:.2f}',
-                'Desvio Padrão F1-Score': '-',
+                'F1-Score Std Dev': '-',
             })
 
-        # Calcular e adicionar a média e o desvio padrão ao final de cada porcentagem
-        media_acuracia = np.mean(acuracias_por_porcentagem)
-        media_f1_score = np.mean(f1_scores_por_porcentagem)
+        # Calculate and add average and standard deviation at the end of each percentage
+        avg_accuracy = np.mean(accuracies_by_percentage)
+        avg_f1_score = np.mean(f1_scores_by_percentage)
         
-        desvio_padrao_acuracia = np.std(acuracias_por_porcentagem)
-        desvio_padrao_f1_score = np.std(f1_scores_por_porcentagem)
+        std_dev_accuracy = np.std(accuracies_by_percentage)
+        std_dev_f1_score = np.std(f1_scores_by_percentage)
 
         results.append({
-            'Porcentagem de Treino': porcentagem,
-            'Temporada': '-',
-            'Acurácia': f'{media_acuracia:.2f}',
-            'Desvio Padrão Acurácia': f'{desvio_padrao_acuracia:.2f}',
-            'F1-Score': f'{media_f1_score:.2f}',
-            'Desvio Padrão F1-Score': f'{desvio_padrao_f1_score:.2f}',
+            'Training Percentage': percentage,
+            'Season': '-',
+            'Accuracy': f'{avg_accuracy:.2f}',
+            'Accuracy Std Dev': f'{std_dev_accuracy:.2f}',
+            'F1-Score': f'{avg_f1_score:.2f}',
+            'F1-Score Std Dev': f'{std_dev_f1_score:.2f}',
         })
 
-        path = os.path.join(base_path, 'results', 'experiment_01', f'{modelo}_experiment_01_{jogos_media}.csv')
+        path = os.path.join(base_path, 'results', 'experiment_01', f'{model}_experiment_01_{games_average}.csv')
         save_results_csv(path, results)
 
     results = []
 
-# Fim do temporizador
+# End the timer
 end_time = time.time()
-print(f"Tempo total de execução: {end_time - start_time:.2f} segundos")
+print(f"Total execution time: {end_time - start_time:.2f} seconds")
--- scripts/experiment_02.py
+++ scripts/experiment_02.py
@@ -4,128 +4,123 @@ import re
 
 import numpy as np
 
-from neural_network import run_model_neural_network
-from svm import run_model_svm 
 from experiments import save_results_csv
 from xgboost_model import run_model_xgboost
 
-modelo = 'xgb'
+model = 'xgb'
 
-bests_params = []
+best_params_list = []
 
 def extract_number(file_name):
-    # Extrai o número do nome do arquivo.
+    # Extract the number from the file name.
     match = re.search(r'(\d+)', file_name)
     return int(match.group(1)) if match else -1
 
 def run_models(data_dir):
-    # Obter a lista de arquivos no diretório de treino e teste
+    # Get the list of files in the train and test directory
     files = os.listdir(data_dir)
 
-    # Filtrar e ordenar os arquivos de treino e teste com base nos números extraídos
-    treino_files = sorted([f for f in files if f.startswith('treino_') and f.endswith('.csv')], key=extract_number)
-    teste_files = sorted([f for f in files if f.startswith('teste_') and f.endswith('.csv')], key=extract_number)
+    # Filter and sort training and testing files based on the extracted numbers
+    train_files = sorted([f for f in files if f.startswith('treino_') and f.endswith('.csv')], key=extract_number)
+    test_files = sorted([f for f in files if f.startswith('teste_') and f.endswith('.csv')], key=extract_number)
 
-    # Garantir que o número de arquivos de treino e teste é o mesmo
-    if len(treino_files) != len(teste_files):
-        print("Número de arquivos de treino e teste não coincide.")
+    # Ensure the number of training and testing files matches
+    if len(train_files) != len(test_files):
+        print("Number of training and testing files does not match.")
         return
 
-    acuracias_from_temporada = []
-    f1_scores_from_temporada = []
+    accuracies_by_season = []
+    f1_scores_by_season = []
 
-    # Loop pelos arquivos de treino e teste
-    for treino_file, teste_file in zip(treino_files, teste_files):
+    # Loop through training and testing files
+    for train_file, test_file in zip(train_files, test_files):
+        train_path = os.path.join(data_dir, train_file)
+        test_path = os.path.join(data_dir, test_file)
 
-        treino_path = os.path.join(data_dir, treino_file)
-        teste_path = os.path.join(data_dir, teste_file)
+        accuracy, f1, best_params = run_model_xgboost(train_path, test_path, True)
 
-        acuracia, f1, best_params = run_model_xgboost(treino_path, teste_path, True)
-
-        if acuracia is None:
+        if accuracy is None:
             continue
         else:
-            bests_params.append(best_params)
-            acuracias_from_temporada.append(acuracia)
-            f1_scores_from_temporada.append(f1)
+            best_params_list.append(best_params)
+            accuracies_by_season.append(accuracy)
+            f1_scores_by_season.append(f1)
     
-    media_acuracia = np.mean(acuracias_from_temporada)
-    media_f1_score = np.mean(f1_scores_from_temporada)
+    avg_accuracy = np.mean(accuracies_by_season)
+    avg_f1_score = np.mean(f1_scores_by_season)
 
-    return media_acuracia, media_f1_score
+    return avg_accuracy, avg_f1_score
 
 if __name__ == '__main__':
-    # Início do temporizador
+    # Start the timer
     start_time_all = time.time()
 
-    # Array para guardar os resultados de cada temporada / janela
+    # Array to store the results of each season / window
     results = []
 
-    temporadas = ['2008-2009', '2009-2010', '2011-2012',
-                  '2012-2013', '2013-2014', '2014-2015',
-                  '2015-2016', '2016-2017', '2018-2019', '2019-2020',
-                  '2020-2021', '2021-2022', '2022-2023',
-                  '2023-2024'
-                 ]
+    seasons = [
+        '2008-2009', '2009-2010', '2011-2012',
+        '2012-2013', '2013-2014', '2014-2015',
+        '2015-2016', '2016-2017', '2018-2019', '2019-2020',
+        '2020-2021', '2021-2022', '2022-2023',
+        '2023-2024'
+    ]
     
-    numeros_jogos_treino = [16]
-    numeros_jogos_teste = [4,6,8]
+    train_games_counts = [16]
+    test_games_counts = [4, 6, 8]
 
     base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
-    for numero_jogos_treino in numeros_jogos_treino:
-        for numero_jogos_teste in numeros_jogos_teste:
+    for train_games_count in train_games_counts:
+        for test_games_count in test_games_counts:
 
-            f1_scores_temporadas = []
-            acuracias_temporadas = []
+            f1_scores_all_seasons = []
+            accuracies_all_seasons = []
 
-            for temporada in temporadas:    
-                data_dir = os.path.join(base_path, "data", "experiment_02", temporada, f'{numero_jogos_treino}'+'-'+f'{numero_jogos_teste}')
+            for season in seasons:
+                data_dir = os.path.join(base_path, "data", "experiment_02", season, f'{train_games_count}-{test_games_count}')
 
-                # Rodando modelos para uma temporada
-                print(f'Rodando modelo para temporada {temporada}, com janela {numero_jogos_treino} - {numero_jogos_teste}')
+                # Running models for one season
+                print(f'Running model for season {season}, with window {train_games_count} - {test_games_count}')
 
-                acuracia_from_temporada, f1_score_from_temporada = run_models(data_dir)
+                accuracy_by_season, f1_score_by_season = run_models(data_dir)
                 
-                acuracias_temporadas.append(acuracia_from_temporada)
-                f1_scores_temporadas.append(f1_score_from_temporada)
+                accuracies_all_seasons.append(accuracy_by_season)
+                f1_scores_all_seasons.append(f1_score_by_season)
 
                 results.append({
-                    'Janela Flutuante': f'{numero_jogos_treino}'+'-'+f'{numero_jogos_teste}',
-                    'Temporada': temporada,
-                    'Acurácia': f'{acuracia_from_temporada:.2f}',
-                    'Desvio Padrão Acurácia': '-',
-                    'F1-Score': f'{f1_score_from_temporada:.2f}',
-                    'Desvio Padrão F1-Score': '-',
+                    'Sliding Window': f'{train_games_count}-{test_games_count}',
+                    'Season': season,
+                    'Accuracy': f'{accuracy_by_season:.2f}',
+                    'Accuracy Std Dev': '-',
+                    'F1-Score': f'{f1_score_by_season:.2f}',
+                    'F1-Score Std Dev': '-',
                 })
 
                 output_dir = os.path.join(base_path, 'results', 'experiment_02')
-                output_path = os.path.join(output_dir, f'{modelo}_experiment_02_treino_{numero_jogos_treino}_parcial.csv')
+                output_path = os.path.join(output_dir, f'{model}_experiment_02_train_{train_games_count}_partial.csv')
                 save_results_csv(output_path, results)
             
-            # Calcular e adicionar a média e o desvio daquela janela para todas as temporadas
-            media_acuracia = np.mean(acuracias_temporadas)
-            media_f1_score = np.mean(f1_scores_temporadas)
+            # Calculate and add the average and std dev for that window across all seasons
+            avg_accuracy = np.mean(accuracies_all_seasons)
+            avg_f1_score = np.mean(f1_scores_all_seasons)
 
-            desvio_padrao_acuracia = np.std(acuracias_temporadas)
-            desvio_padrao_f1_score = np.std(f1_scores_temporadas)
+            std_dev_accuracy = np.std(accuracies_all_seasons)
+            std_dev_f1_score = np.std(f1_scores_all_seasons)
 
             results.append({
-                'Janela Flutuante': f'{numero_jogos_treino}'+'-'+f'{numero_jogos_teste}',
-                'Temporada': '-',
-                'Acurácia': f'{media_acuracia:.2f}',
-                'Desvio Padrão Acurácia': f'{desvio_padrao_acuracia:.2f}',
-                'F1-Score': f'{media_f1_score:.2f}',
-                'Desvio Padrão F1-Score': f'{desvio_padrao_f1_score:.2f}',
+                'Sliding Window': f'{train_games_count}-{test_games_count}',
+                'Season': '-',
+                'Accuracy': f'{avg_accuracy:.2f}',
+                'Accuracy Std Dev': f'{std_dev_accuracy:.2f}',
+                'F1-Score': f'{avg_f1_score:.2f}',
+                'F1-Score Std Dev': f'{std_dev_f1_score:.2f}',
             })
 
             output_dir = os.path.join(base_path, 'results', 'experiment_02')
-            output_path = os.path.join(output_dir, f'{modelo}_experimento_02_treino_{numero_jogos_treino}.csv')
+            output_path = os.path.join(output_dir, f'{model}_experiment_02_train_{train_games_count}.csv')
             save_results_csv(output_path, results)
 
-    # Fim do temporizador
+    # End the timer
     end_time_all = time.time()
-    print(f"Tempo total de execução: {end_time_all - start_time_all:.2f} segundos")
-
-
-
+    print(f"Total execution time: {end_time_all - start_time_all:.2f} seconds")
